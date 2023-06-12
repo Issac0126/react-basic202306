3451fdfc4ad31154b786f06c2d161bc48dcb4ac5
LeeYoonSeo <diano679@gmail.com>
2023-06-12 13:07:04 +0900

    2306012 컴포넌트 기초 완료

---
+++ src/SayHello.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+//컴포넌트
+//컴포넌트 파일은 대문자로 작성하는 것이 관례이다.
+// 함수명은 파일명과 동일하게 작성해주면 좋다.
+function SayHello(){
+    return(
+        <div>
+            <p>안녕</p>
+            <span>룰루</span>
+        </div>
+    );
+}
+
+export default SayHello;
